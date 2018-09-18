3e5eb356e6f8555942d316f3aa9d3b71d45ce0f1
Jeffrey Chamberlain <jchamberlain@stfrancishs.org>
2018-09-18 17:21:04 +0000

    Mr. Chamberlain's crazy instructions

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,27 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    for (k=0;k<5;k++)
+    //for (k=0;k<5;k++)
+    while(k<5)
     {
-        sum+=k; //use something different in "while" version
+        //sum+=k; //use something different in "while" version
+        printf("%d\n",k);
+        sum = sum + k;
+        k++;
     }
 
-    printf ("%d\n",sum);
+    printf ("Sum - %d\n",sum);
 
-    k=5;
+    //k=5;
 
-    while (k>0) {
-        k=k-1; //use something different in "for" version
-        printf ("%d\n", k);
+    //while (k>0)
+    for (k=10;k>0;k--)
+    {
+        //k=k-1; //use something different in "for" version
+        printf ("%2d\n", k);
     }
-
+    printf ("Blastoff!");
 }
 
